99357350174f3c424a5085ef6a977553b6dd7abb
Antti Salonen <ajsalonen@gmail.com>
2010-07-13 22:36:00 +0200

    preliminary additions to build system

--- Setup.hs
+++ Setup.hs
@@ -1,15 +1,35 @@
 import System.IO
+import System.Exit
+import System.FilePath
 
-import Distribution.Simple
 import Distribution.PackageDescription
+import Distribution.Simple
+import Distribution.Simple.Program
 import Distribution.Simple.LocalBuildInfo
 import Distribution.Simple.Setup
 
 main = defaultMainWithHooks $ simpleUserHooks
   {
-    buildHook = testBuildHook
+    hookedPrograms = [simpleProgram "cgen-hs"]
+  , buildHook = testBuildHook
   }
 
+err :: String -> IO a
+err str = hPutStrLn stderr str >> exitWith (ExitFailure 1)
+
+getSrcFile :: PackageDescription -> String -> IO FilePath
+getSrcFile pd ext = do
+  let files = filter (\f -> takeExtension f == (extSeparator:ext)) $ extraSrcFiles pd
+  case files of
+    [file] -> return file
+    files  -> err $ "Error in extra source files configuration: Exactly one file with the extension \"" ++ ext ++ "\" allowed. Found:\n\t" ++ show files
+
 testBuildHook :: PackageDescription -> LocalBuildInfo -> UserHooks -> BuildFlags -> IO ()
-testBuildHook pd lb uh bf = (buildHook simpleUserHooks) pd lb uh bf
+testBuildHook pd lb uh bf = do
+  iffile   <- getSrcFile pd "if"
+  igfile   <- getSrcFile pd "ig"
+  hiffile  <- getSrcFile pd "hif"
+  listfile <- getSrcFile pd "list"
+  
+  (buildHook simpleUserHooks) pd lb uh bf
 
