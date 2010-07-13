f9a2addb7809f592c09e8b44001e6c6a21d1217b
Antti Salonen <ajsalonen@gmail.com>
2010-07-13 22:08:00 +0200

    preparations for 0.1.0 - cgen binding generation

--- Setup.hs
+++ Setup.hs
@@ -1,3 +1,15 @@
+import System.IO
+
 import Distribution.Simple
-main = defaultMain
+import Distribution.PackageDescription
+import Distribution.Simple.LocalBuildInfo
+import Distribution.Simple.Setup
+
+main = defaultMainWithHooks $ simpleUserHooks
+  {
+    buildHook = testBuildHook
+  }
+
+testBuildHook :: PackageDescription -> LocalBuildInfo -> UserHooks -> BuildFlags -> IO ()
+testBuildHook pd lb uh bf = (buildHook simpleUserHooks) pd lb uh bf
 
