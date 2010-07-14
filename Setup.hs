d1225d523f35ec315ba1d4043302177162203ddf
Antti Salonen <ajsalonen@gmail.com>
2010-07-14 10:38:10 +0200

    add generation of the c wrapper by running cgen and pkg-config

--- Setup.hs
+++ Setup.hs
@@ -1,16 +1,19 @@
 import System.IO
 import System.Exit
 import System.FilePath
+import Control.Applicative
+import Control.Monad
 
 import Distribution.PackageDescription
 import Distribution.Simple
 import Distribution.Simple.Program
 import Distribution.Simple.LocalBuildInfo
 import Distribution.Simple.Setup
+import Distribution.Verbosity
 
 main = defaultMainWithHooks $ simpleUserHooks
   {
-    hookedPrograms = [simpleProgram "cgen-hs"]
+    hookedPrograms = [simpleProgram "cgen", simpleProgram "cgen-hs"]
   , buildHook = testBuildHook
   }
 
@@ -24,12 +27,29 @@ getSrcFile pd ext = do
     [file] -> return file
     files  -> err $ "Error in extra source files configuration: Exactly one file with the extension \"" ++ ext ++ "\" allowed. Found:\n\t" ++ show files
 
+getProgram :: String -> String -> IO ConfiguredProgram
+getProgram src pname = do
+  mloc <- findProgramLocation normal pname
+  case mloc of
+    Nothing  -> err $ "Could not find program \"" ++ pname ++ "\".\n" ++ 
+                      "Try installing the program from " ++ src ++ ".\n" ++ 
+                      "If you've installed the program, make sure the program is in your PATH."
+    Just loc -> return $ ConfiguredProgram pname Nothing [] (FoundOnSystem loc)
+
 testBuildHook :: PackageDescription -> LocalBuildInfo -> UserHooks -> BuildFlags -> IO ()
 testBuildHook pd lb uh bf = do
   iffile   <- getSrcFile pd "if"
   igfile   <- getSrcFile pd "ig"
   hiffile  <- getSrcFile pd "hif"
   listfile <- getSrcFile pd "list"
-  
-  (buildHook simpleUserHooks) pd lb uh bf
+  cgen <- getProgram "Hackage" "cgen"
+  pkgconfig <- getProgram "http://pkg-config.freedesktop.org/" "pkg-config"
+  headerlist <- lines <$> readFile listfile
+  mogreincpath <- (filter (\w -> take 2 w == "-I") . words) <$> getProgramOutput normal pkgconfig ["--cflags", "OGRE"]
+  case mogreincpath of
+    []    -> err "Could not find OGRE include path with pkg-config - make sure OGRE is installed and pkg-config configured."
+    (x:_) -> do
+      let ogreincpath = drop 2 x
+      runProgram normal cgen (["-o", "res", "--interface", iffile, "--include", ogreincpath] ++ headerlist)
+      (buildHook simpleUserHooks) pd lb uh bf
 
